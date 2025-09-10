0b38b22021a7764d4c5daa8215f17c7067531a03
Guillermo Soliz Nagatoshi <guiye15@gmail.com>
2025-09-10 01:05:53 -0400

    feat: experiencias FOTO

--- src/components/experience/experienceData.ts
+++ src/components/experience/experienceData.ts
@@ -7,7 +7,7 @@ export const experienceData = [
         responsibilities: ["Desarrollo de sistemas web", "Mantenimiento de aplicaciones", "Soporte técnico", "Arquitectura de software", "Liderazgo de equipo"],
         achievements: ["Software de facturación electrónica", "Software de Avaluos", "Software de RRHH", "Software de Afiliaciones y Comisiones", "Software de Control de Produccion"],
         tech: [".Net Framework", "Sql Server", "vb.net", "Java Swing", "Android", "Controles Telerik", "MySQL"],
-        logo: "/public/experience/logo-dte.png"
+        logo: "/experience/logo-dte.png"
     },
     {
         company: "Banco Ganadero S.A.",
@@ -17,7 +17,7 @@ export const experienceData = [
         responsibilities: ["Desarrollo de microservicios", "Integración con Kafka", "CI/CD con Jenkins", "Liderazgo de equipo", "Coordinación con otros equipos"],
         achievements: ["Autenticación JWT", "Loyalty Program", "Microservicios para Banca Móvil", "Integración con sistemas core bancarios", "Automatización de despliegues"],
         tech: [".Net Core", "Node.js", "MongoDB", "Kafka", "Jenkins", "Azure DevOps", "Docker", "Kubernetes", "Azure", "SOAP"],
-        logo: "/public/experience/bg.png"
+        logo: "/experience/bg.png"
     },
   // Más experiencias...
 ];
